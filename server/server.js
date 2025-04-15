6f0f7e700243a29690fda731278fbfee7fee257d
Brandon Cortes <brandon.cortes@o2ebrands.com>
2025-04-15 15:15:20 -0500

    frontend ui react

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+
 import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
 
 import productRoutes from './routes/productRoutes.js'
 import { sql } from './config/db.js'
@@ -13,14 +15,31 @@ dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
+
+console.log(__dirname);
+
 
 
 app.use(express.json())
 app.use(cors())
-app.use(helmet())
+app.use( 
+    helmet({
+        contentSecurityPolicy: false,
+    })
+)
 app.use(morgan('dev'))
 
 app.use('/api/products', productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  // server our react app
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
  async function initDB() {
     try {
